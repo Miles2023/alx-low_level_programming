0dfc09baaf1ef96adc4b3f7eeb43057a99800cbb
Ogunboye Blessing <alabaogunboye@gmail.com>
2022-12-10 06:53:37 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- *  * main - Entry                                                                                                                         
- *   * Return: Always 0                                                                                                                     
- *    */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
-			        return (0);
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multi lingual puzzle");
+	return (0);
+
 }
