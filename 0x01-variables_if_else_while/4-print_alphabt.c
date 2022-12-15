decb4c986b831ae85feb0840dc767fe15ed30b39
Ogunboye Blessing <alabaogunboye@gmail.com>
2022-12-15 08:00:40 -0800

    4print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower and upper case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+
+		if (la != 'e' && la != 'q')
+			putchar(la)
+	}
+
+	putchar('\n');
+
+
+	return (0);
+
+}
