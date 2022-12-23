7ac0b4c79ce9ddad342a0408f939ebe1e7fbcee7
Ogunboye Blessing <alabaogunboye@gmail.com>
2022-12-22 20:58:06 -0800

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,31 +6,20 @@ void print_uint(unsigned int n);
 */
 void print_number(int n)
 {
-
 if (n < 0)
-
 {
-
 _putchar('-');
-
 n = -n;
-
 }
-
 print_uint((unsigned int) n);
-
 }
-
 /**
 * print_uint - Prints an unsigned integer
 * @n: The unsigned int to print
 */
 void print_uint(unsigned int n)
-
 {
-
 if (n / 10 != 0)
-
 print_uint(n / 10);
 
 _putchar(n % 10 + '0');
