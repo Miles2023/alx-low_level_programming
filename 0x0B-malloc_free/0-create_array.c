95037fe2e5931910e12dfd2bf942cfcb9dfb90b6
Ogunboye Blessing <alabaogunboye@gmail.com>
2023-01-11 05:28:58 -0800

    tasks all

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * create_array - function that creates an array of chars
+ * and initializes it with a specific char.
+ * @size: number elements in the array
+ * @c: char
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arrMem;
+	unsigned int i;
+
+	if (size <= 0)
+	{
+
+		return (NULL);
+	}
+	else
+	{
+		arrMem = malloc(size * sizeof(char));
+		if (arrMem == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+			{
+				*(arrMem + i) = c;
+			}
+			return (arrMem);
+		}
+	}
+
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strdup - function that returns a pointer to newly allocated space in memory
+ * which contains a copy of the string given as a parameter.
+ * @str: string to duplicate
+ * Return: a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+
+{
+
+	char *dupl;
+	int i, j;
+
+	if (str == NULL)
+
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+
+		;
+
+	dupl = malloc(sizeof(char) * (i + 1));
+
+	if (dupl == NULL)
+
+		return (NULL);
+
+	for (j = 0; (dupl[j] = str[j]) != '\0'; j++)
+
+		;
+
+	dupl[i] = '\0';
+
+	return (dupl);
+
+}
